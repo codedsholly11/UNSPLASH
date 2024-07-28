b48ae556c29e512f69914c09ffb688838fd0f513
codedsholly11 <codedsholly@gmail.com>
2024-07-28 14:43:18 +0100

    start responsiveness

--- src/App.js
+++ src/App.js
@@ -26,9 +26,19 @@ const App = () => {
 
     }
   return (
-    <div className='flex flex-col gap-[10px]'>
-      <input onChange={(e)=> setSearch(e.target.value)}  className='w-[20vw] h-[7vh] border-2 rounded-lg' type='text'></input>
-      <button onClick={fetchApi}  className='bg-cyan-300 px-[10px] h-[7vh] w-[10vw] rounded-lg'>{loading? 'submitting' :'submit'}</button>
+    <div className='flex flex-col gap-[10px] h-[100vh] w-[100vw] items-center pt-[20px] px-[20px] md:bg-white bg-purple-100'>
+    <div className=''>
+      <div className=''>
+        <h1 className='font-sans text-4xl md:text-center text-start py-[20px] '>fancy<span className='text-purple-700'>pix</span></h1>
+      </div>
+      <div className='flex flex-col gap-3 items-center'>
+        <input onChange={(e)=> setSearch(e.target.value)}  className='md:w-[20vw] w-[90vw] md:h-[7vh] h-[10vh] border-2 rounded-lg px-[10px]' type='text'placeholder='search for images'></input>
+        <button onClick={fetchApi}  className='bg-cyan-300 px-[10px] md:h-[7vh] h-[8vh] md:w-[10vw] w-[90vw] rounded-lg text-2xl '>{loading? 'submitting' :'submit'}</button>
+      </div>
+    </div>
+      <div className='px-[] text-xl text-center text-green-600'>
+        <p> fancy<span className='text-purple-700'>pix. </span>your go-to place for premium images that beatify and elevate your works</p>
+      </div>
      {search && (
       <div>
       {images.map((image,index) => (
@@ -38,8 +48,10 @@ const App = () => {
        ))}
       </div>
      )
-
      }
+
+
+     <h1 className='text-purple-700 pb-[15px]'>Designed By:- @CODEDSHOLLY</h1>
     </div>
   )
 }
