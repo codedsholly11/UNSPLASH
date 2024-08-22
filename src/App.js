45a8cf96d328b6884e5ca88353d96b6ff8757f7d
codedsholly11 <codedsholly@gmail.com>
2024-08-22 12:38:49 +0100

    responsiveness concluded

--- src/App.js
+++ src/App.js
@@ -26,24 +26,24 @@ const App = () => {
 
     }
   return (
-    <div className='flex flex-col gap-[10px] h-[100vh] w-[100vw] items-center pt-[20px] px-[20px] md:bg-white bg-purple-100'>
+    <div className='flex flex-col gap-[10px] h-[100%] w-[100%] items-center  px-[20px] md:bg-white bg-purple-100'>
     <div className=''>
       <div className=''>
         <h1 className='font-sans text-4xl md:text-center text-start py-[20px] '>fancy<span className='text-purple-700'>pix</span></h1>
       </div>
       <div className='flex flex-col gap-3 items-center'>
-        <input onChange={(e)=> setSearch(e.target.value)}  className='md:w-[20vw] w-[90vw] md:h-[7vh] h-[10vh] border-2 rounded-lg px-[10px]' type='text'placeholder='search for images'></input>
+        <input onChange={(e)=> setSearch(e.target.value)}  className='md:w-[20vw] w-[100%] md:h-[7vh] h-[10vh] border-2 rounded-lg px-[10px]' type='text'placeholder='search for images'></input>
         <button onClick={fetchApi}  className='bg-cyan-300 px-[10px] md:h-[7vh] h-[8vh] md:w-[10vw] w-[90vw] rounded-lg text-2xl '>{loading? 'submitting' :'submit'}</button>
       </div>
     </div>
       <div className='px-[] text-xl text-center text-green-600'>
-        <p> fancy<span className='text-purple-700'>pix. </span>your go-to place for premium images that beatify and elevate your works</p>
+        <p> fancy<span className='text-purple-700'>pix. </span>your go-to place for premium images that beautify and elevate your works</p>
       </div>
      {search && (
-      <div>
+      <div className='flex flex-wrap items-center justify-center gap-[20px]'>
       {images.map((image,index) => (
-         <div key={index}>
-           <img src={image.urls.small} alt={image.description}/>
+         <div className='w-[95%] h-[200px]' key={index}>
+           <img src={image.urls.small} className='h-[100%] w-[100%]' alt={image.description}/>
          </div>
        ))}
       </div>
